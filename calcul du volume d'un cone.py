c3132c1c2d8d1d3aa0385585f0c80d97e4404bd7
unknown <samregal29@gmail.com>
2022-12-28 14:37:08 +0100

    ok

---
+++ calcul du volume d'un cone.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+print("Calcul du volume d'un cone droit")
+r=float(input("Entrez le rayon :"))
+h=float(input("Entrez la hauteur :"))
+      
+v=1/3*(3.14*r*r)*h         
+print("le volume du cone est :", v)
